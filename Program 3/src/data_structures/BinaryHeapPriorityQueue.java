8f4fdee4b26144a7eb720c37d5bd6c99ee71e2a7
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-06 16:58:47 -0700

    Work on insert method: -> upHeap()

--- Program 3/src/data_structures/BinaryHeapPriorityQueue.java
+++ Program 3/src/data_structures/BinaryHeapPriorityQueue.java
@@ -1,13 +1,53 @@
 package data_structures;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class BinaryHeapPriorityQueue<E extends Comparable<E>> implements PriorityQueue<E> {
 
+    /*
+    * Variables
+    * - arrayList stores the all the entries
+    * - capacity is the max capacity of the list
+    * - last index the index of the last element of the heap
+    * */
+    private E[] arrayList;
+    private int capacity;
+    private int lastIndex;
+    private Comparator<E> comparator;
+
+    /*
+    * Constructors
+    * - One Parameter : comparator
+    * - Two Parameter : comparator, capacity
+    * */
+
+    BinaryHeapPriorityQueue(Comparator<E> comparator) {
+        this.arrayList = (E[]) new Object[DEFAULT_MAX_CAPACITY];
+        this.capacity = DEFAULT_MAX_CAPACITY;
+        this.lastIndex = -1;
+        this.comparator = comparator;
+    }
+    BinaryHeapPriorityQueue(int capacity, Comparator<E> comparator) {
+        this.arrayList = (E[]) new Object[capacity];
+        this.capacity = capacity;
+        this.lastIndex = -1;
+        this.comparator = comparator;
+    }
+
+
 
     public boolean insert(E object) {
+        if (isFull()) {return false;}
+        arrayList[lastIndex+1] = object;
+        //if-heapViolation then upHeap
+        upHeap(object);
+
         return false;
+    }
+
+    private void upHeap(E object) {
     }
 
     public E remove() {
