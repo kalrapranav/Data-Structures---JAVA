236842af120fbb2b6a66d1273de569d1fcd84111
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-06 16:03:41 -0700

    implement methods from the abstract class

--- Program 3/src/data_structures/BinaryHeapPriorityQueue.java
+++ Program 3/src/data_structures/BinaryHeapPriorityQueue.java
@@ -1,8 +1,48 @@
 package data_structures;
 
 import java.util.Collection;
+import java.util.Iterator;
 
-public class BinaryHeapPriorityQueue {
+public class BinaryHeapPriorityQueue<E extends Comparable<E>> implements PriorityQueue<E> {
 
-  
+
+    public boolean insert(E object) {
+        return false;
+    }
+
+    public E remove() {
+        return null;
+    }
+
+    public boolean delete(E obj) {
+        return false;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public boolean contains(E obj) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        return null;
+    }
 }
