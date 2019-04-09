67c760905badb6de833348e2d1b0b94627137b12
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-09 03:51:11 -0700

    Work on generic variables and other methods

--- Program 3/src/data_structures/BinaryHeapPriorityQueue.java
+++ Program 3/src/data_structures/BinaryHeapPriorityQueue.java
@@ -2,38 +2,101 @@
 package data_structures;
 
 
+import java.util.Comparator;
+import java.util.Iterator;
 
-public class BinaryHeapPriorityQueue {
-    private int[] array;
-    private int pos;
+public class BinaryHeapPriorityQueue<E> implements PriorityQueue {
+    private E[] array;
     private int capacity;
+    private int lastIndex;
+    private Comparator<E> comparator;
 
-    public BinaryHeapPriorityQueue() {
-        pos = 1;
-        capacity = 10;
-        array = new int[capacity];
+    public BinaryHeapPriorityQueue(int capacity, Comparator<E> comparator) {
+        this.array = (E[]) new Object[capacity];
+        this.capacity = capacity;
+        this.lastIndex = -1;
+        this.comparator = comparator;
     }
 
-    public void insert(int object) {
-        array[pos++] = object;
+    private void bottomUpHeapify(int index) {
+        //@// TODO: 11/12/16 This implementation is not optimized
+        int parentIndex = getParent(index);
 
-        int child = pos-1;
-        int parent = child/2;
-
-        while(array[parent] > array[child] && parent > 0) {
-            int tmp = array[parent];
-            array[parent] = array[child];
-            array[child] = tmp;
-
-            child = parent;
-            parent = child/2;
+        /** Checking if we reached at parent or not. **/
+        if (index == 0 || !isValidIndex(parentIndex)) {
+            return;
         }
+
+        E childElement = array[index];
+        E parentElement = array[parentIndex];
+
+        /** Comparing parent element with child **/
+        int result = comparator.compare(parentElement, childElement);
+
+        /** If parent is less than or equal to child then don't do anything. **/
+        if (result <= 0) {
+            return;
+        }
+
+        /** If parent is greater than child then, swap value **/
+        array[index] = parentElement;
+        array[parentIndex] = childElement;
+
+        /** And recursively validate parent with grandparent **/
+        bottomUpHeapify(parentIndex);
     }
 
-    public void print() {
-        for(int i=1; i<pos; i++) {
-            System.out.print(array[i] + " ");
+    private int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= lastIndex;
+    }
+
+    public boolean insert(Comparable object) {
+        if (isFull()) {
+            throw new RuntimeException("Heap is full.");
         }
+        array[++lastIndex] = (E) object;
+        bottomUpHeapify(lastIndex);
+        return true;
+    }
+    
+    public Comparable remove() {
+        return null;
+    }
+
+    public boolean delete(Comparable obj) {
+        return false;
+    }
+
+    public Comparable peek() {
+        return null;
+    }
+
+    public boolean contains(Comparable obj) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return lastIndex + 1 == capacity;
+    }
+
+    public Iterator iterator() {
+        return null;
     }
 
 
