a64f905528b6c88d426cad28233356892be32b76
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-10 22:04:59 -0700

    All tests passed - citations left

--- Program 3/src/data_structures/BinaryHeapPriorityQueue.java
+++ Program 3/src/data_structures/BinaryHeapPriorityQueue.java
@@ -1,93 +1,73 @@
-//WORK IN PROGRESS
 package data_structures;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class BinaryHeapPriorityQueue <E extends Comparable<E>> implements PriorityQueue<E>, Iterable<E> {
+public class BinaryHeapPriorityQueue <E extends Comparable<E>> implements PriorityQueue<E>{
 
     private Wrapper<E>[] array;
     long modificationCounter = 0;
     int currentSize = 0;
     int totalArraySize;
     public long entryNumber;
-    //int DEFAULT_CAPACITY = DEFAULT_MAX_CAPACITY;
 
-    public BinaryHeapPriorityQueue() {
+    public BinaryHeapPriorityQueue(){
         this(DEFAULT_MAX_CAPACITY);
     }
-
-    public BinaryHeapPriorityQueue(int size) {
+    public BinaryHeapPriorityQueue(int size){
         totalArraySize = size;
         currentSize = 0;
         entryNumber = 0;
         array = new Wrapper[size];
     }
 
-    public class Wrapper<E> implements Comparable<Wrapper<E>> {
+    public class Wrapper<E> implements Comparable<Wrapper<E>>{
         E data;
         public long sequenceNumber;
 
-        public Wrapper(E obj) {
+        public Wrapper(E obj){
             data = obj;
             sequenceNumber = entryNumber++;
         }
-
-        public int compareTo(Wrapper<E>obj) {
+        public int compareTo(Wrapper<E>obj){
             int tmp = ((Comparable<E>)data).compareTo(obj.data);
-            if(tmp == 0)
-                return (int) (sequenceNumber - obj.sequenceNumber);
+            if(tmp ==0)
+                return (int)(sequenceNumber - obj.sequenceNumber);
             return tmp;
         }
-        public String toString() {
-            return "" + data;
+        public String toString(){
+            return ""+data;
         }
     }
 
     public boolean insert(E object) {
-        if (currentSize == totalArraySize)
-            return false;
+        if(currentSize == totalArraySize) return false;
         array[currentSize] = new Wrapper<>(object);
         int oldSize = currentSize;
         currentSize++;
+        modificationCounter++;
         trickleUp(oldSize);
         return true;
     }
 
-
-    private void trickleUp(int index) {
-//        int parent = (int) ((index - 1)/2);
-
-        index = currentSize-1;
-        int parent = (int) ((index - 1) >> 1);
+    public void trickleUp( int index){
+        modificationCounter++;
+        int parent =  (int)((index-1)/2);
         Wrapper<E> currentObject = array[index];
-
-        while ((parent >= 0) &&
-                (currentObject.compareTo(array[parent]) < 0)) {
+        while ((parent >= 0) && (currentObject.compareTo(array[parent]) < 0)) {
             array[index] = array[parent];
-            index = parent;
-            parent = (parent - 1)>>1;
+            index =  parent;
+            parent = (index-1)>>1;
         }
         array[index] = currentObject;
     }
 
-    public E remove() {
-        if(isEmpty())
-            return null;
-        E top = array[0].data;
-        //bring last element at the top
-
-        trickleDown(0);
-        currentSize = currentSize--;
+    public void trickleDown(int index){
         modificationCounter++;
-        return top;
-    }
-
-    public void trickleDown(int oldSize) {
         int current = 0;
         int child = getNextChild(current);
-        while (child != -1 && array[current].compareTo(array[child]) < 0 && array[child].compareTo(array[currentSize - 1]) < 0) {
+        while (child != -1 && array[current].compareTo(array[child]) < 0 && array[child].compareTo(array[currentSize - 1]) < 0){
             array[current] = array[child];
             current = child;
             child = getNextChild(current);
@@ -95,52 +75,59 @@ public class BinaryHeapPriorityQueue <E extends Comparable<E>> implements Priori
         array[current] = array[currentSize - 1];
     }
 
-    private int getNextChild(int current) {
-        int left = (current * 2) + 1;
-        int right = left + 1;
-        if (right < currentSize) {
-            if (array[left].compareTo(array[right]) < 0)
+    public int getNextChild(int current){
+        int left = (current * 2)+1;
+        int right = left+1;
+        if(right < currentSize){
+            if(array[left].compareTo(array[right]) < 0)
                 return left;
             return right;
         }
-        if (left < currentSize)
-            return left;
+        if (left < currentSize) return left;
         return -1;
     }
 
+    public E remove() {
+        int temp = currentSize;
+        if(isEmpty()){return null;}
+        E top = array[0].data;
+        trickleDown(0);
+        temp--;
+        modificationCounter++;
+        currentSize--;
+        return top;
+    }
+
     public boolean delete(E obj) {
-        if(!contains(obj))
-            return false;
+        if(!contains(obj)) return false;
         Wrapper<E> [] tempStorage = array;
         int size = currentSize;
         boolean removed = false;
         currentSize = 0;
         entryNumber = 0;
-        for (int i = 0; i < size; i++){
-            if ((tempStorage[i].data).compareTo(obj) != 0) {
+        for (int i =0; i < size; i++)
+            if ((tempStorage[i].data).compareTo(obj) != 0)
                 insert(tempStorage[i].data);
-            }
             else {
                 modificationCounter++;
                 removed = true;
             }
-        }
-
         return removed;
     }
 
     public E peek() {
-        if (isEmpty())
+        if(isEmpty()){
             return null;
+        }
         return array[0].data;
     }
 
     public boolean contains(E obj) {
-        if(isEmpty())
-            return false;
-        for(Wrapper<E> anyObject : array) {
-            if (anyObject.data.compareTo(obj) == 0)
+        if(isEmpty())return false;
+        for(int i=0; i<currentSize;i++ ){
+            if (array[i].data.compareTo(obj)==0){
                 return true;
+            }
         }
         return false;
     }
@@ -152,16 +139,18 @@ public class BinaryHeapPriorityQueue <E extends Comparable<E>> implements Priori
     public void clear() {
         currentSize = 0;
         entryNumber = 0;
+
+
     }
 
     public boolean isEmpty() {
-        if (currentSize == 0)
+        if(currentSize == 0)
             return true;
         return false;
     }
 
     public boolean isFull() {
-        if (currentSize == totalArraySize)
+        if(currentSize == totalArraySize)
             return true;
         return false;
     }
@@ -169,33 +158,29 @@ public class BinaryHeapPriorityQueue <E extends Comparable<E>> implements Priori
     public Iterator<E> iterator() {
         return new IteratorHelper();
     }
-
-    private class IteratorHelper implements Iterator<E> {
+    public class IteratorHelper implements Iterator<E>{
         private int current = 0;
         private long oldModificationCounter = modificationCounter;
-
-        @Override
         public boolean hasNext() {
-            if (oldModificationCounter != modificationCounter) {
-                throw new ConcurrentModificationException("");
+            if(oldModificationCounter != modificationCounter){
+                throw new ConcurrentModificationException("yeet yeet dont change the skeet!");
             }
-            if (current < currentSize)
+            if(current < currentSize){
                 return true;
+            }
             return false;
         }
 
-        @Override
         public E next() {
-            if (!hasNext()) {
+            if(!hasNext()){
                 throw new NoSuchElementException();
             }
             return array[current++].data;
         }
 
-        @Override
         public void remove() {
             throw new UnsupportedOperationException();
         }
     }
-
 }
+
