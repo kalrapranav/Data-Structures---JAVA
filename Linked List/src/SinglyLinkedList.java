40f282a04dad04d09a754d77e39f57af4b2f0e1e
Pranav Kalra <pranav99kalra@gmail.com>
2019-05-01 09:51:40 -0700

    Class variables,constructor,and innner class completed

--- Linked List/src/SinglyLinkedList.java
+++ Linked List/src/SinglyLinkedList.java
@@ -1,7 +1,39 @@
 public class SinglyLinkedList<E> implements LinkedListInterface<E> {
+
+    class Node<E> {
+        E data;
+        Node<E> next;
+        Node<E> previous;
+        //constructor
+        public Node(E obj) {
+            data = obj;
+            next = null;
+            previous = null;
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int currentSize;
+
+    //constructor
+    public SinglyLinkedList() {
+        head  = tail = null;
+        currentSize = 0;
+    }
+
+    /*
+    * In every method, take care of the five boundary conditions:
+    * 1. Empty
+    * 2. Single Element
+    * 3. Working in Middle
+    * 4. Working at the beginning
+    * 5. Working at the end
+    * */
+
     @Override
     public void addFirst(E obj) {
-        
+
     }
 
     @Override
