a1af235280c97fb5a22ed48fd06410d2a63efe63
Pranav Kalra <pranav99kalra@gmail.com>
2019-05-01 11:33:40 -0700

    Linked list tester created

--- Linked List/src/LinkedListTester.java
+++ Linked List/src/LinkedListTester.java
@@ -1,2 +1,6 @@
-public class LinkedListTester {
+public class LinkedListTester<E> {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        linkedList.addFirst(12);
+    }
 }
