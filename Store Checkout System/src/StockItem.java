2e68a5d03c5b5270965f237a2150497a3874a96f
Pranav Kalra <pranav99kalra@gmail.com>
2019-05-05 02:04:31 -0700

    changes to stockItem class

--- Store Checkout System/src/StockItem.java
+++ Store Checkout System/src/StockItem.java
@@ -10,22 +10,22 @@ public class StockItem implements Comparable<StockItem> {
     float retail;
 
     // Constructor.  Creates a new StockItem instance.
-    public StockItem(String SKU, String description, String vendor,float cost, float retail)
+    public StockItem(String SKU, String description, String vendor,float cost, float retail){}
 
     // Follows the specifications of the Comparable Interface.
     // The SKU is always used for comparisons, in dictionary order.
 
-    public int compareTo(StockItem n)
+    public int compareTo(StockItem n) {return 0;}
     // Returns an int representing the hashCode of the SKU.
 
-    public int hashCode()
+    public int hashCode() {return 0;}
     // standard get methods
 
-    public String getDescription()
-    public String getVendor()
-    public float getCost()
-    public float getRetail()
+    public String getDescription() {return null;}
+    public String getVendor() {return null;}
+    public float getCost() {return 0;}
+    public float getRetail() {return 0;}
 
     // All fields in one line, in order
-    public String toString()
+    public String toString() {return null;}
 }
