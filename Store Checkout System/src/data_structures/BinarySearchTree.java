5759aaeb306c867554f0dc26975da8907f6c043c
Pranav Kalra <pranav99kalra@gmail.com>
2019-05-06 23:40:06 -0700

    inner Node class for BST completed

--- Store Checkout System/src/data_structures/BinarySearchTree.java
+++ Store Checkout System/src/data_structures/BinarySearchTree.java
@@ -3,6 +3,30 @@ package data_structures;
 import java.util.Iterator;
 
 public class BinarySearchTree<K extends Comparable<K>, V> implements DictionaryADT<K,V>{
+    /**
+     * Binary Search Tree is gonna use array as a medium to store data
+     * so, the complexity of add, remove and find operation becomes O(1)
+     * for all these functions we will use the key as teh index
+     * */
+
+    /**
+     * Node class will be used to assign the Key and value for the different
+     * positions of the tree. As, the constructor of the class takes K,V and
+     * assigns it to Key and Value
+     * */
+    private class Node {
+        private K key;
+        private V val;
+        private Node left;
+        private Node right;
+        private int count;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
     @Override
     public boolean contains(Comparable key) {
         return false;
